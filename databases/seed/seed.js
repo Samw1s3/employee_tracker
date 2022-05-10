99c0bde26e7002724055eade7607ca57dcd4d865
Sam Knopper <samanthaknopper@hotmail.com>
2022-05-10 21:49:03 +0800

    create schema fixed, tables created

--- databases/seed/seed.js
+++ databases/seed/seed.js
@@ -5,13 +5,13 @@
 //create 10 departments
 
 const { seedDepartment } = require("./department-seed");
-
+const { seedRoles } = require("./role-seed")
 
 async function seed() {
     // seed department
     await seedDepartment(10);
     // seed roles
-
+    // await seedRoles(10);
     // seed employee
 }
 
---
+++ schema/seeds.sql
@@ -0,0 +1,24 @@
+INSERT INTO roles (title, salary, department_id) 
+VALUES  (customer service, 65000, 1);
+        (sales lead, 100000, 1);
+        (technian, 75000, 2);
+        (technian supervisor, 110000, 2);
+        (engineer, 90000, 3);
+        (lead engineer, 85000, 3);
+        (marketing director, 170000, 4);
+        (marketing team leader, 150000, 4);
+        (human resource assistant, 65000, 5);
+        (human resource manager, 110000, 5);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id) 
+VALUES  (Joe, Black, 1, 2);
+        (Jaden, Smith, 2,);
+        (Julie, Hart, 3, 4);
+        (Zac, Clark, 4);
+        (Phil, Upton, 5, 6);
+        (Mike, Perez, 6);
+        (Kim, Dawson, 7, 8);
+        (Leah, Nelson, 8);
+        (Jim, Kinder, 9, 10);
+        (Joel, Nolan, 10);
+
