56640036a03dcf8e79d0c27ff0098662fab0944a
Sam Knopper <samanthaknopper@hotmail.com>
2022-05-11 19:10:14 +0800

    seeds created

--- schema/seeds.sql
+++ schema/seeds.sql
@@ -1,24 +1,37 @@
-INSERT INTO roles (title, salary, department_id) 
-VALUES  (customer service, 65000, 1);
-        (sales lead, 100000, 1);
-        (technian, 75000, 2);
-        (technian supervisor, 110000, 2);
-        (engineer, 90000, 3);
-        (lead engineer, 85000, 3);
-        (marketing director, 170000, 4);
-        (marketing team leader, 150000, 4);
-        (human resource assistant, 65000, 5);
-        (human resource manager, 110000, 5);
+INSERT INTO departments(name)
+VALUES 
+("Management"),
+("Sales"),
+("Warehouse"),
+("Human Resource"),
+("Quality Control"),
+("Marketing"),
+("Accounting");
 
-INSERT INTO employees (first_name, last_name, role_id, manager_id) 
-VALUES  (Joe, Black, 1, 2);
-        (Jaden, Smith, 2,);
-        (Julie, Hart, 3, 4);
-        (Zac, Clark, 4);
-        (Phil, Upton, 5, 6);
-        (Mike, Perez, 6);
-        (Kim, Dawson, 7, 8);
-        (Leah, Nelson, 8);
-        (Jim, Kinder, 9, 10);
-        (Joel, Nolan, 10);
+INSERT INTO roles(title, salary, department_id) 
+VALUES  ("Regional Manager", 200000, 1),
+        ("customer service", 65000, 2),
+        ("sales lead", 100000, 2),
+        ("technian", 75000, 3),
+        ("Quality Control Officer", 110000, 5),
+        ("engineer", 90000, 3),
+        ("lead engineer", 85000, 3),
+        ("marketing director", 170000, 6),
+        ("marketing team leader", 150000, 6),
+        ("human resource represenative", 110000, 4);
 
+
+
+INSERT INTO employees (first_name, last_name, role_id) 
+VALUES  ("Joe", "Black", 1),
+        ("Jaden", "Smith", 2),
+        ("Julie", "Hart", 3),
+        ("Zac", "Clark", 4),
+        ("Phil", "Upton", 5),
+        ("Mike", "Perez", 6),
+        ("Kim", "Dawson", 7),
+        ("Leah", "Nelson", 8),
+        ("Jim", "Kinder", 9),
+        ("Joel", "Nolan", 10);
+
+UPDATE employees SET manager_id = 1 WHERE (id > 1);
